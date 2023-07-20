f7d6eed1b0acf7c9172da1917c0064fda6d7c7ac
Peter Woods <peter@peterwoods.io>
2023-07-20 13:24:14 -0700

    Initial commit with boilerplate Xcode App project

---
+++ MovieApp/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  MovieApp
+//
+//  Created by Peter Woods on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
---
+++ MovieAppTests/MovieAppTests.swift
@@ -0,0 +1,13 @@
+//
+//  MovieAppTests.swift
+//  MovieAppTests
+//
+//  Created by Peter Woods on 7/19/23.
+//
+
+import XCTest
+@testable import MovieApp
+
+final class MovieAppTests: XCTestCase {
+
+}
