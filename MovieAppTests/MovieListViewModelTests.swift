ff525da25b03254210fae9401aa8ab6f8e6bb9d7
Peter Woods <peter@peterwoods.io>
2023-07-20 13:24:15 -0700

    Add unit tests for MovieListViewModel

---
+++ MovieAppTests/MovieListViewModelTests.swift
@@ -0,0 +1,83 @@
+//
+//  MovieListViewModelTests.swift
+//  MovieAppTests
+//
+//  Created by Peter Woods on 7/20/23.
+//
+
+import XCTest
+@testable import MovieApp
+
+/// `MovieListViewModel` tests.
+final class MovieListViewModelTests: XCTestCase {
+    func testInitialState() {
+        let viewModel = MovieListViewModel(movieProvider: emptyProvider)
+
+        XCTAssertFalse(viewModel.isErrorShown)
+        XCTAssertFalse(viewModel.isNoResultsShown)
+    }
+
+    func testEmptyQuery() async {
+        let viewModel = MovieListViewModel(movieProvider: emptyProvider)
+
+        await viewModel.fetchMovies(query: "")
+
+        XCTAssertFalse(viewModel.isErrorShown)
+        XCTAssertFalse(viewModel.isNoResultsShown)
+    }
+
+    func testEmptyResponse() async {
+        let viewModel = MovieListViewModel(movieProvider: emptyProvider)
+
+        await viewModel.fetchMovies(query: "Test")
+
+        XCTAssertFalse(viewModel.isErrorShown)
+        XCTAssertTrue(viewModel.isNoResultsShown)
+    }
+
+    func testErrorResponse() async {
+        let viewModel = MovieListViewModel(movieProvider: errorProvider)
+
+        await viewModel.fetchMovies(query: "Test")
+
+        XCTAssertTrue(viewModel.isErrorShown)
+        XCTAssertFalse(viewModel.isNoResultsShown)
+    }
+
+    func testValidResponse() async {
+        let viewModel = MovieListViewModel(movieProvider: validProvider)
+
+        await viewModel.fetchMovies(query: "Hitman")
+
+        XCTAssertFalse(viewModel.isErrorShown)
+        XCTAssertFalse(viewModel.isNoResultsShown)
+
+        XCTAssertEqual(viewModel.movies.count, 3)
+    }
+}
+
+private extension MovieListViewModelTests {
+    var emptyProvider: MovieProvider {
+        struct Provider: MovieProvider {
+            func fetchMovies(searchTerm: String) async -> [Movie]? {
+                return []
+            }
+        }
+
+        return Provider()
+    }
+
+    var errorProvider: MovieProvider {
+        struct Provider: MovieProvider {
+            func fetchMovies(searchTerm: String) async -> [Movie]? {
+                return nil
+            }
+        }
+
+        return Provider()
+    }
+
+    var validProvider: MovieProvider {
+        return JSONMovieProvider(json: JSONMovieProvider.sampleMovieList)
+    }
+}
